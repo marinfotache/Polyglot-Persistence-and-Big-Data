5e38c74cb4f793cdb2c6ee2399b357339f44c3a0
MarinFotache <marinfotache@gmail.com>
2019-05-14 22:27:28 +0300

    Add files via upload

---
+++ 06 Graph databases. Neo4j/06-03a_export_sales__pg_to_neo4j.sql
@@ -0,0 +1,171 @@
+--############################################################################
+--### 	      Export `sales` database from PostgreSQL to Neo4j 
+--#############################################################################
+
+-- database `sales` in already created and populated 
+-- see scripts `01...` and `02...` on github:
+-- https://github.com/marinfotache/Polyglot-Persistence-and-Big-Data/tree/master/04%20JSON%20Data%20Management%20in%20SQL%20Data%20Servers.%20PostgreSQL/mini%20case%20study%20-%20sales
+
+-- connect to the (sub)schema sales with pgAdmin IV and run the following statements
+
+
+DROP TABLE IF EXISTS  sales__pg_to_neo4j ;
+CREATE TABLE sales__pg_to_neo4j (
+	attrib varchar(5000)) ;
+
+
+-- as in `sales` database the number of records is not so big, 
+-- we can try to create the nodes and relationships in a single 
+-- statement (which is not affordable when dealing with large databases)
+DELETE FROM sales__pg_to_neo4j ;
+
+INSERT INTO sales__pg_to_neo4j SELECT text FROM (
+
+	-- 1. Create `:Region` nodes
+   SELECT DISTINCT 1 AS order, 
+   		'CREATE (region_' || LTRIM(RTRIM(region)) || ':Region {name:"' || region || '"})' 
+   		AS text 
+   	FROM counties	
+	
+	UNION
+	
+	-- 2. Link `:County` nodes to `:Region` nodes
+   SELECT 2 AS order, 
+   		'CREATE (county_'|| LTRIM(RTRIM(countycode)) ||':County {code:"' || countycode || '", name:"' || 
+					countyname || '"}) ' ||
+			 ' -[:CountyInRegion]-> ' ||  
+			 ' (region_' || LTRIM(RTRIM(region)) || ') ' 
+   		AS text 
+   	FROM counties	
+	
+	UNION
+		
+	-- 3. Link `:PostalCode` nodes to `:County` nodes
+   SELECT 3 AS order, 
+   		'CREATE (postcode_' || LTRIM(RTRIM(postcode))  || ':PostalCode {post_code:"' || postcode || 
+					'", town:"' || place || '"}) ' ||
+			 ' -[:PostalCodeInCounty]-> ' ||  
+			 ' (county_'|| LTRIM(RTRIM(countycode)) || ') ' 
+   		AS text 
+   	FROM postcodes 
+
+	UNION
+	
+	-- 4. Link `:Customer` nodes to `:PostalCode` nodes
+	--   Notice: some attributes in table CUSTOMER are nullable  
+   SELECT 4 AS order, 
+		'CREATE (customer_' || customerid || ':Customer { cust_id:' || customerid || 
+				COALESCE(', cust_name:"' || customername, '') ||
+     			COALESCE('", fiscal_code:"' || fiscalcode, '') || 
+				COALESCE('", cust_address:"' || address, '') ||
+     			COALESCE('", cust_phone:"' || phone, '') || '"})'   ||   
+			 ' -[:LocatedIn]-> ' ||
+			 ' (postcode_' || LTRIM(RTRIM(postcode)) || ') ' 
+   		AS text 
+   	FROM customers 
+
+	UNION
+	
+	-- 5. Create `:Category` nodes (Category names are composed of only letters, numbers and spaces)
+   SELECT DISTINCT 5 AS order, 
+   		'CREATE (category_' || LTRIM(RTRIM(REPLACE(category, ' ', '_'))) || ':Category {category:"' || category || '"})' 
+   		AS text 
+   	FROM products	
+
+	UNION
+
+	-- 6. Link `:Product` nodes to `:Category` nodes
+   SELECT 6 AS order, 
+   		'CREATE (product_'|| productid ||':Product { product_id:' || productid || ', product_name:"' || productname ||
+		     '", measure_unit:"' || unitofmeasurement || 
+     		'", current_vat_percent: ' || vatpercent * 100 || ' }) ' ||
+			 ' -[:ProductInCategory]-> ' ||  
+			' (category_' || LTRIM(RTRIM(REPLACE(category, ' ', '_'))) || ') '    AS text 
+   	FROM products	
+	
+	UNION
+
+	-- 7. Link `:Invoice` nodes to `:Customer` nodes
+	--   Notice: `comments` attribute in table INVOICES is nullable and `invoicedare`
+	--  is of type `date`
+   SELECT 7 AS order, 
+		'CREATE (invoice_' || invoiceno || ':Invoice { invoice_id: ' || invoiceno || 
+		    ', invoice_date: date("' || invoicedate || '")' || 
+			COALESCE(', comments:"' || comments || '"', '') || ' }) ' ||
+			' -[:SentTo]-> ' ||
+			 ' (customer_' || customerid || ') ' 
+	   		AS text 
+   	FROM invoices 
+	
+	UNION
+
+	-- 8. Link `:Invoice` nodes to `:Product` nodes
+	--   Notice: attributes of table INVOICE_DETAILS will be transformed into
+	-- properties of `:InvoiceDetails` relationships
+   SELECT 8 AS order, 
+		'CREATE (invoice_' || invoiceno || ') '
+			' -[ :InvoiceDetails {row_number: ' || invoicerownumber || 
+			', quantity: ' || quantity || ', unit_price:' || unitprice || 
+			'} ] -> ' ||
+			 ' (product_' || productid || ') ' 
+	   		AS text 
+   	FROM invoice_details 
+
+	UNION
+
+	-- 9. Create `:Receipt` nodes 
+   SELECT 9 AS order, 
+		'CREATE (receipt_' || receiptid || ':Receipt { receipt_id: ' || receiptid || 
+		    ', receipt_date: date("' || receiptdate || '")' || 
+	    	', doc_type:"' || doctype || '", doc_number:"' || docnumber ||
+		    '", doc_date: date("' || docdate || '")})'  
+			AS text
+   	FROM receipts 
+
+	UNION
+
+	-- 10. Link `:Receipt` nodes to `:Invoice` nodes
+   SELECT 10 AS order, 
+		'CREATE (receipt_' || receiptid || ') '
+			' -[ :ReceiptPaysInvoice {amount: ' || amount || '} ] -> ' ||
+			 ' (invoice_' || invoiceno || ') ' 
+	   		AS text 
+   	FROM receipt_details 
+	
+	ORDER BY 1 ) t ;
+
+SELECT * FROM sales__pg_to_neo4j ;
+
+
+--############################################################################
+/* 						Next, in PgAdmin, 
+	1.a click on the table `sales__pg_to_neo4j`
+	1.b righ-click, and choose `Import/Export`, then 'Export`
+	1.c choose `Format` as `text` and save it a accessibile directory
+	
+The result must ressemble to the content of the file
+`06-03b_export_sales_pg_to_neo4j.txt`
+
+
+--############################################################################
+/* 						Next, in  `Neo4j Desktop`
+//
+// 2.a. Run `Neo4j Desktop` on your computer
+//
+// 2.b. Open the project created in previous scripts (or create a new project)
+//
+// 2.c. After opening the project, click on `Add Graph` box, then choose `Create a Local Graph`;
+//		set `Graph name` property on `sales`, fill in the password, then click on button `Create`
+// 
+// 2.d. After the database creation completed, press `Start` button in `books` box. 
+//
+// 2.e. After the database is started, press `Manage` button, then `Open browser` button
+//
+// 2.f. Copy all the file content created in step 1.c into Neo4j Browswer. Job done (if everything runs smoothly)
+// 
+
+
+*/
+
+
+
