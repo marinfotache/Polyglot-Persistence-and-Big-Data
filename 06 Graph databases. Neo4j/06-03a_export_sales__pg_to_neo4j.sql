1b0c520baf4791557a16fafc7055bac5f1a0e839
MarinFotache <marinfotache@gmail.com>
2019-05-14 23:37:00 +0300

    Add files via upload

--- 06 Graph databases. Neo4j/06-03a_export_sales__pg_to_neo4j.sql
+++ 06 Graph databases. Neo4j/06-03a_export_sales__pg_to_neo4j.sql
@@ -8,40 +8,33 @@
 
 -- connect to the (sub)schema sales with pgAdmin IV and run the following statements
 
+DROP TABLE IF EXISTS sales__pg_to_neo4j ;
 
-DROP TABLE IF EXISTS  sales__pg_to_neo4j ;
-CREATE TABLE sales__pg_to_neo4j (
-	attrib varchar(5000)) ;
-
-
--- as in `sales` database the number of records is not so big, 
--- we can try to create the nodes and relationships in a single 
--- statement (which is not affordable when dealing with large databases)
-DELETE FROM sales__pg_to_neo4j ;
-
-INSERT INTO sales__pg_to_neo4j SELECT text FROM (
+CREATE TABLE sales__pg_to_neo4j AS
+SELECT text FROM (
+			SELECT * FROM (
 
 	-- 1. Create `:Region` nodes
-   SELECT DISTINCT 1 AS order, 
+   SELECT DISTINCT 1 AS order_in_result, 
    		'CREATE (region_' || LTRIM(RTRIM(region)) || ':Region {name:"' || region || '"})' 
    		AS text 
-   	FROM counties	
+   	FROM (SELECT * FROM counties ORDER BY region )counties
 	
 	UNION
 	
 	-- 2. Link `:County` nodes to `:Region` nodes
-   SELECT 2 AS order, 
+   SELECT 2 AS order_in_result, 
    		'CREATE (county_'|| LTRIM(RTRIM(countycode)) ||':County {code:"' || countycode || '", name:"' || 
 					countyname || '"}) ' ||
 			 ' -[:CountyInRegion]-> ' ||  
 			 ' (region_' || LTRIM(RTRIM(region)) || ') ' 
    		AS text 
-   	FROM counties	
+   	FROM (SELECT * FROM counties ORDER BY countyname ) counties	
 	
 	UNION
 		
 	-- 3. Link `:PostalCode` nodes to `:County` nodes
-   SELECT 3 AS order, 
+   SELECT 3 AS order_in_result, 
    		'CREATE (postcode_' || LTRIM(RTRIM(postcode))  || ':PostalCode {post_code:"' || postcode || 
 					'", town:"' || place || '"}) ' ||
 			 ' -[:PostalCodeInCounty]-> ' ||  
@@ -51,9 +44,28 @@ INSERT INTO sales__pg_to_neo4j SELECT text FROM (
 
 	UNION
 	
-	-- 4. Link `:Customer` nodes to `:PostalCode` nodes
+	-- 4. Create `:Category` nodes (Category names are composed of only letters, numbers and spaces)
+   SELECT DISTINCT 4 AS order_in_result, 
+   		'CREATE (category_' || LTRIM(RTRIM(REPLACE(category, ' ', '_'))) || ':Category {category:"' || category || '"})' 
+   		AS text 
+   	FROM products	
+
+	UNION
+
+	-- 5. Link `:Product` nodes to `:Category` nodes
+   SELECT 5 AS order_in_result, 
+   		'CREATE (product_'|| productid ||':Product { product_id:' || productid || ', product_name:"' || productname ||
+		     '", measure_unit:"' || unitofmeasurement || 
+     		'", current_vat_percent: ' || vatpercent * 100 || ' }) ' ||
+			 ' -[:ProductInCategory]-> ' ||  
+			' (category_' || LTRIM(RTRIM(REPLACE(category, ' ', '_'))) || ') '    AS text 
+   	FROM products	
+	
+	UNION
+				
+	-- 6. Link `:Customer` nodes to `:PostalCode` nodes
 	--   Notice: some attributes in table CUSTOMER are nullable  
-   SELECT 4 AS order, 
+   SELECT 6 AS order_in_result, 
 		'CREATE (customer_' || customerid || ':Customer { cust_id:' || customerid || 
 				COALESCE(', cust_name:"' || customername, '') ||
      			COALESCE('", fiscal_code:"' || fiscalcode, '') || 
@@ -65,30 +77,11 @@ INSERT INTO sales__pg_to_neo4j SELECT text FROM (
    	FROM customers 
 
 	UNION
-	
-	-- 5. Create `:Category` nodes (Category names are composed of only letters, numbers and spaces)
-   SELECT DISTINCT 5 AS order, 
-   		'CREATE (category_' || LTRIM(RTRIM(REPLACE(category, ' ', '_'))) || ':Category {category:"' || category || '"})' 
-   		AS text 
-   	FROM products	
-
-	UNION
-
-	-- 6. Link `:Product` nodes to `:Category` nodes
-   SELECT 6 AS order, 
-   		'CREATE (product_'|| productid ||':Product { product_id:' || productid || ', product_name:"' || productname ||
-		     '", measure_unit:"' || unitofmeasurement || 
-     		'", current_vat_percent: ' || vatpercent * 100 || ' }) ' ||
-			 ' -[:ProductInCategory]-> ' ||  
-			' (category_' || LTRIM(RTRIM(REPLACE(category, ' ', '_'))) || ') '    AS text 
-   	FROM products	
-	
-	UNION
 
 	-- 7. Link `:Invoice` nodes to `:Customer` nodes
 	--   Notice: `comments` attribute in table INVOICES is nullable and `invoicedare`
 	--  is of type `date`
-   SELECT 7 AS order, 
+   SELECT 7 AS order_in_result, 
 		'CREATE (invoice_' || invoiceno || ':Invoice { invoice_id: ' || invoiceno || 
 		    ', invoice_date: date("' || invoicedate || '")' || 
 			COALESCE(', comments:"' || comments || '"', '') || ' }) ' ||
@@ -102,7 +95,7 @@ INSERT INTO sales__pg_to_neo4j SELECT text FROM (
 	-- 8. Link `:Invoice` nodes to `:Product` nodes
 	--   Notice: attributes of table INVOICE_DETAILS will be transformed into
 	-- properties of `:InvoiceDetails` relationships
-   SELECT 8 AS order, 
+   SELECT 8 AS order_in_result, 
 		'CREATE (invoice_' || invoiceno || ') '
 			' -[ :InvoiceDetails {row_number: ' || invoicerownumber || 
 			', quantity: ' || quantity || ', unit_price:' || unitprice || 
@@ -114,7 +107,7 @@ INSERT INTO sales__pg_to_neo4j SELECT text FROM (
 	UNION
 
 	-- 9. Create `:Receipt` nodes 
-   SELECT 9 AS order, 
+   SELECT 9 AS order_in_result, 
 		'CREATE (receipt_' || receiptid || ':Receipt { receipt_id: ' || receiptid || 
 		    ', receipt_date: date("' || receiptdate || '")' || 
 	    	', doc_type:"' || doctype || '", doc_number:"' || docnumber ||
@@ -125,16 +118,22 @@ INSERT INTO sales__pg_to_neo4j SELECT text FROM (
 	UNION
 
 	-- 10. Link `:Receipt` nodes to `:Invoice` nodes
-   SELECT 10 AS order, 
+   SELECT 10 AS order_in_result, 
 		'CREATE (receipt_' || receiptid || ') '
 			' -[ :ReceiptPaysInvoice {amount: ' || amount || '} ] -> ' ||
 			 ' (invoice_' || invoiceno || ') ' 
 	   		AS text 
    	FROM receipt_details 
-	
-	ORDER BY 1 ) t ;
+
+			) x
+			ORDER BY 1, 2 
+		) y
+	;
 
 SELECT * FROM sales__pg_to_neo4j ;
+
+
+
 
 
 --############################################################################
