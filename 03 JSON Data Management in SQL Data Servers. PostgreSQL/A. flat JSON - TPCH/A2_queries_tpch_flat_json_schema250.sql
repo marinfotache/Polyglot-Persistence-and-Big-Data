a9d49625fe5b465dd9ccad9b7717128c3102aeea
MarinFotache <marinfotache@gmail.com>
2020-03-30 19:30:56 +0300

    Add files via upload

--- 03 JSON Data Management in SQL Data Servers. PostgreSQL/A. flat JSON - TPCH/A2_queries_tpch_flat_json_schema250.sql
+++ 03 JSON Data Management in SQL Data Servers. PostgreSQL/A. flat JSON - TPCH/A2_queries_tpch_flat_json_schema250.sql
@@ -105,3 +105,47 @@ FROM sf_0_01__schema_250__flat_JSON.nation
 	INNER JOIN sf_0_01__schema_250__flat_JSON.region
 		ON cast(nation ->> 'n_regionkey' as integer) =
 			cast (region ->> 'r_regionkey' as integer) ;
+
+
+			SELECT count(*) FROM sf_0_01__schema_250__flat_JSON.lineitem_orders_customer
+
+			SELECT count(*) FROM sf_0_01__schema_250__flat_JSON.partsupp_supplier_nation
+
+			------------------------------------------------------
+			-- 		get the geografical region of each supplier
+			------------------------------------------------------
+
+
+			-- 1. in the original fully normalized TPCH schema the solution was:
+			select * from region
+			select * from nation
+			select * from supplier
+
+			select s_name, n_name, r_name
+			from supplier
+				inner join nation on supplier.s_nationkey = nation.n_nationkey
+				inner join region on nation.n_regionkey = region.r_regionkey
+			order by 1
+
+
+
+			-- 2 in our schema, the tables are different, so:
+			select * from sf_0_01__schema_250__flat_JSON.partsupp_supplier_nation ;
+			select * from sf_0_01__schema_250__flat_JSON.region ;
+
+
+
+			select distinct
+				partsupp_supplier_nation ->> 's_name' as supplier_name,
+				partsupp_supplier_nation ->> 'n_name' as country_name,
+				region ->> 'r_name' as region_name
+			from sf_0_01__schema_250__flat_JSON.partsupp_supplier_nation
+				inner join sf_0_01__schema_250__flat_JSON.region
+				on partsupp_supplier_nation ->> 'n_regionkey' = region ->> 'r_regionkey'
+			order by 1
+
+
+
+			------------------------------------------------------
+			-- 		get the geografical region of each customer
+			------------------------------------------------------
