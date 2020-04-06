9a5707b0a0b5e7685b379626be9abfae0bbaf192
MarinFotache <marinfotache@gmail.com>
2020-04-06 08:09:55 +0300

    Add files via upload

--- 03 JSON Data Management in SQL Data Servers. PostgreSQL/C. nested JSON - TPCH/C1_create_populate_tables_tpch_nested_json_schema250.sql
+++ 03 JSON Data Management in SQL Data Servers. PostgreSQL/C. nested JSON - TPCH/C1_create_populate_tables_tpch_nested_json_schema250.sql
@@ -6,13 +6,19 @@
 -- JSON attribute with nested array
 ----------------------------------------------------------
 
+
+-- First, connect (with PgAdmin) to `tpch_0_01` database
+
+-- ...then launch, one by one, the following statements:
+
+
  DROP SCHEMA IF EXISTS sf_0_01__schema_250__pure_JSON CASCADE ;
  CREATE SCHEMA sf_0_01__schema_250__pure_JSON;
 -- path1: lineitem_orders_customer-nation-region
 -- path2: lineitem-partsupp-part
 -- path3: lineitem-partsupp_supplier_nation-region
 
---      Schema tables are:
+-- Schema tables are:
 --     lineitem_orders_customer
 --     nation
 --     region
@@ -29,66 +35,63 @@ from
           (select to_jsonb(array_agg(to_jsonb(orders_new)))
            from
                (select orders.*,
-          (select to_jsonb(array_agg(to_jsonb(lineitem_new)))
-           from
-               (select lineitem.*
-                from lineitem
-                where lineitem.l_orderkey = orders.o_orderkey
-               ) lineitem_new
-         ) lineitem
+                    (select to_jsonb(array_agg(to_jsonb(lineitem_new)))
+                     from
+                        (select lineitem.*
+                         from lineitem
+                         where lineitem.l_orderkey = orders.o_orderkey
+                         ) lineitem_new
+                    ) lineitem
                 from orders
                 where orders.o_custkey = customer.c_custkey
                ) orders_new
          ) orders
-     from customer) customer_new WITH NO DATA ;
+     from customer
+   ) customer_new WITH NO DATA ;
 
 DO $$
   declare
     rec RECORD ;
 begin
     for rec in (select * from customer) loop
-     insert into sf_0_01__schema_250__pure_JSON.lineitem_orders_customer
-select to_jsonb(customer_new) as customer
-from
-     (select customer.*,
-          (select to_jsonb(array_agg(to_jsonb(orders_new)))
-           from
-               (select orders.*,
-          (select to_jsonb(array_agg(to_jsonb(lineitem_new)))
-           from
-               (select lineitem.*
-                from lineitem
-                where lineitem.l_orderkey = orders.o_orderkey
-               ) lineitem_new
-         ) lineitem
-                from orders
-                where orders.o_custkey = customer.c_custkey
-               ) orders_new
-         ) orders
-     from customer) customer_new
-   where c_custkey = rec.c_custkey;
-    end loop ;
-     end ; $$
+        insert into sf_0_01__schema_250__pure_JSON.lineitem_orders_customer
+          select to_jsonb(customer_new) as customer
+          from
+              (select customer.*,
+                    (select to_jsonb(array_agg(to_jsonb(orders_new)))
+                     from
+                          (select orders.*,
+                                (select to_jsonb(array_agg(to_jsonb(lineitem_new)))
+                                 from
+                                      (select lineitem.*
+                                       from lineitem
+                                       where lineitem.l_orderkey = orders.o_orderkey
+                                       ) lineitem_new
+                                ) lineitem
+                          from orders
+                          where orders.o_custkey = customer.c_custkey
+                        ) orders_new
+                    ) orders
+              from customer
+            ) customer_new
+          where c_custkey = rec.c_custkey;
+  end loop ;
+end ; $$
 ;
 
 CREATE TABLE sf_0_01__schema_250__pure_JSON.nation AS
-
   SELECT to_jsonb(nation) AS nation FROM  nation;
 
 CREATE TABLE sf_0_01__schema_250__pure_JSON.region AS
-
   SELECT to_jsonb(region) AS region FROM  region;
 
 CREATE TABLE sf_0_01__schema_250__pure_JSON.lineitem AS
-
   SELECT to_jsonb(lineitem) AS lineitem FROM  lineitem;
 
 CREATE TABLE sf_0_01__schema_250__pure_JSON.partsupp AS
-
   SELECT to_jsonb(partsupp) AS partsupp FROM  partsupp;
 
 CREATE TABLE sf_0_01__schema_250__pure_JSON.part AS
-
   SELECT to_jsonb(part) AS part FROM  part;
 
 CREATE TABLE sf_0_01__schema_250__pure_JSON.partsupp_supplier_nation AS
